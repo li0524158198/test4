cd292a926e5047098b538ef47015cfdde2042bba
li0524158198 <li0524158198@163.com>
2014-04-10 18:28:42 +0800

    Initial Commit

---
+++ MySegNav/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  MySegNav
+//
+//  Created by liweihua on 14-4-10.
+//  Copyright (c) 2014年 lwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySegNav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySegNav
+//
+//  Created by liweihua on 14-4-10.
+//  Copyright (c) 2014年 lwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
